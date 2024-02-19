6e701843f43bf46f6b5b66eae4b54c63d61b690a
Priyanka Nandii <priyanka15-11155@diu.edu.bd>
2024-02-19 23:42:47 +0600

    modal created

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ let booking = 40;
 for (const btn of allBtn) {
   btn.addEventListener("click", function (e) {
     count = count + 1;
-    console.log(e.target.parentNode.childNode);
-
-
     setInnerText("booking-seats", count);
+     
+
     if (count > 4) {
-    //   alert("you cant buy more than 4");
-    //   count = count - 1;
-    seats[index].target.addAttribute("disabled", true);
+      alert("you cant buy more than 4");
+      count--;
       e.target.addAttribute("disabled", true);
+     
+      return;
+    // seats[index].target.addAttribute("disabled", true);
+    //   
    
     }
     e.target.style.backgroundColor ="#1dd100"
@@ -34,9 +36,7 @@ for (let index = 0; index < seats.length; index++) {
   const seat = seats[index];
   seat.addEventListener("click", function () {
     //    console.log("CLICKED")
-    if(seat.length >4){
-        seats[index].target.addAttribute("disabled", true); 
-    }
+  
     const title = seat.querySelector("h1").innerText;
     const seatType = seat.querySelector("p").innerText;
     const ticketPrice = parseFloat(
@@ -71,9 +71,8 @@ discountBtn.addEventListener("click", function () {
   const couponCode = couponElement.split(" ").join("").toUpperCase();
   console.log(couponCode);
 
-  if (totalPrice === 2200) {
-    seats[index].target.addAttribute("disabled", true);
-
+  if (totalPrice >= 2200) {
+     
     if (couponCode === "NEW15") {
       const discountElement = document.getElementById("discountPrice");
       const discountAmount = totalPrice - totalPrice * 0.15;
