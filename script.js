96d3c706831cef915a9d9033525f31e0a97d6a01
Priyanka Nandii <priyanka15-11155@diu.edu.bd>
2024-02-19 22:32:30 +0600

    booking function created

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 const allBtn = document.getElementsByClassName("bus");
 let count = 0;
 let booking = 40;
- for(const btn of allBtn){
-    btn.addEventListener("click", function(e){   
-        count = count +1;
-        setInnerText("booking-seats", count);
-        booking = booking -1;
-        setInnerText("seat-left", booking);
-        
-    });
- }
+for (const btn of allBtn) {
+  btn.addEventListener("click", function (e) {
+    count = count + 1;
+    console.log(e.target.parentNode.childNode);
 
- function setInnerText(id, value){
-    document.getElementById(id).innerText = value;
- }
 
+    setInnerText("booking-seats", count);
+    if (count > 4) {
+    //   alert("you cant buy more than 4");
+    //   count = count - 1;
+    seats[index].target.addAttribute("disabled", true);
+      e.target.addAttribute("disabled", true);
+   
+    }
+    e.target.style.backgroundColor ="#1dd100"
+    e.target.setAttribute("disabled", true);
+
+    booking = booking - 1;
+    setInnerText("seat-left", booking);
+  });
+}
+
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
 
 let totalPrice = 0;
 const seats = document.querySelectorAll(".seat");
 
-
 for (let index = 0; index < seats.length; index++) {
-    const seat = seats[index];
-    seat.addEventListener("click", function (){
+  const seat = seats[index];
+  seat.addEventListener("click", function () {
     //    console.log("CLICKED")
+    if(seat.length >4){
+        seats[index].target.addAttribute("disabled", true); 
+    }
     const title = seat.querySelector("h1").innerText;
     const seatType = seat.querySelector("p").innerText;
-    const ticketPrice = parseFloat(seat.querySelector("h3").innerText.split(" "));
-   
+    const ticketPrice = parseFloat(
+      seat.querySelector("h3").innerText.split(" ")
+    );
 
     const titleContainer = document.getElementById("title-container");
     const p = document.createElement("p");
@@ -44,55 +58,41 @@ for (let index = 0; index < seats.length; index++) {
     h3.innerText = ticketPrice;
     priceContainer.appendChild(h3);
 
-   
     totalPrice += ticketPrice;
-    document.getElementById("totalPrice").innerText = totalPrice;   
-         
+    document.getElementById("totalPrice").innerText = totalPrice;
 
-    })
     
+  });
 }
 
 const discountBtn = document.getElementById("apply-btn");
-discountBtn.addEventListener("click",function(){
- const couponElement = document.getElementById("discount-input").value;
- const couponCode = couponElement.split(" ").join("").toUpperCase();
-  console.log(couponCode)
+discountBtn.addEventListener("click", function () {
+  const couponElement = document.getElementById("discount-input").value;
+  const couponCode = couponElement.split(" ").join("").toUpperCase();
+  console.log(couponCode);
 
- 
-   
- if(totalPrice >= 2200){
-    // alert("You Can't Buy More Than 4 Tickets");
-    seats[index].setAttribute("disabled" , true);
+  if (totalPrice === 2200) {
+    seats[index].target.addAttribute("disabled", true);
 
+    if (couponCode === "NEW15") {
+      const discountElement = document.getElementById("discountPrice");
+      const discountAmount = totalPrice - totalPrice * 0.15;
+      discountElement.innerText = discountAmount;
 
-    if(couponCode === "NEW15"){
-        const discountElement = document.getElementById("discountPrice");
-        const discountAmount = totalPrice - (totalPrice * 0.15);
-        discountElement.innerText = discountAmount;
+      document.getElementById("discount-input").style.display = "none";
+      document.getElementById("apply-btn").style.display = "none";
+    } else if (couponCode === "COUPLE20") {
+      const discountElement = document.getElementById("discountPrice");
+      const discountAmount = totalPrice - totalPrice * 0.2;
+      discountElement.innerText = discountAmount;
+      document.getElementById("discount-input").value = "";
+    } else {
+      alert("invalid coupon");
 
-        document.getElementById("discount-input").style.display = "none";
-        document.getElementById("apply-btn").style.display = "none";
-
- 
-        
-    }else if(couponCode === "COUPLE20"){
-        const discountElement = document.getElementById("discountPrice");
-        const discountAmount = totalPrice - (totalPrice * 0.2);
-        discountElement.innerText = discountAmount; 
-        document.getElementById("discount-input").value = "";
-        
-    }else {
-        alert("invalid coupon")
-        
-        document.getElementById("discount-input").value = "";
+      document.getElementById("discount-input").value = "";
     }
-   
- }else{
-    alert("Please at least buy 4 tickets")
+  } else {
+    alert("Please at least buy 4 tickets");
     document.getElementById("discount-input").value = "";
- }
+  }
 });
-    
-
-
